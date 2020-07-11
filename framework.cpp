88ceb9fa360414366f81ae471cb5b3fbb1c255da
aqfort <valiev1379@gmail.com>
2020-07-11 16:32:16 +0300

    names

--- framework.cpp
+++ framework.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <sstream>
-#include <exception>
+#include <stdexcept>
 #include <string>
 #include <vector>
 #include <map>
 #include <set>
+#include <random>
 
 using namespace std;
 
@@ -25,47 +26,135 @@ string Join(const Collection& c, char d);
 
 
 template<class T, class U>
-void AssertEqual(const T& t, const U& u, const string& hint = {}) {
-    if (t != u) {
-        ostringstream os;
-        os << "Assertion failed: " << t << " != " << u;
-        if (!hint.empty()) {
-             os << " --- hint: " << hint;
-        }
-        throw runtime_error(os.str());
-    }
-}
+void AssertEqual(const T& t, const U& u, const string& hint);
 
-void Assert(bool b, const string& hint) {
-    AssertEqual(b, true, hint);
-}
+void Assert(bool b, const string& hint);
 
 class TestRunner {
 public:
     template <class TestFunc>
-    void RunTest(TestFunc func, const string& test_name) {
-        try {
-            func();
-            cerr << test_name << " OK" << endl;
-        } catch (exception& e) {
-            ++fail_count;
-            cerr << test_name << " fail: " << e.what() << endl;
-        } catch (...) {
-            ++fail_count;
-            cerr << "Unknown exception caught" << endl;
-        }
-    }
+    void RunTest(TestFunc func, const string& test_name);
 
-    ~TestRunner() {
-        if (fail_count > 0) {
-            cerr << fail_count << " unit tests failed" << endl;
-            exit(1);
-        }
-    }
+    ~TestRunner();
 
 private:
     int fail_count = 0;
 };
+
+void TestAll();
+
+void TestUsual();
+void TestOneRoot();
+void TestNoRoots();
+void TestLinearEquation();
+void TestConstant();
+
+
+
+
+
+int GetDistinctRealRootCount(double a, double b, double c) {
+    double D = 0;
+    D = b * b - 4 * a * c;
+    if (D > 0) {
+        return 2;
+    } else if (D == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+} // Wrong realisation
+
+
+
+
+
+int main() {
+    TestAll();
+
+    return 0;
+}
+
+
+
+
+
+void TestAll() {
+    TestRunner runner;
+    runner.RunTest(TestUsual, "TestUsual");
+    runner.RunTest(TestOneRoot, "TestOneRoot");
+    runner.RunTest(TestNoRoots, "TestNoRoots");
+    runner.RunTest(TestLinearEquation, "TestLinearEquation");
+    runner.RunTest(TestConstant, "TestConstant");
+}
+
+void TestUsual() {
+    default_random_engine generator;
+    uniform_real_distribution<double> distribution(-10, 10);
+
+    double a, b, c;
+    int result;
+
+    for (size_t i = 0; i < 100; i++) {
+        a = distribution(generator);
+        b = distribution(generator);
+        c = distribution(generator);
+
+        result = GetDistinctRealRootCount(a, b, c);
+
+        Assert(result >= 0 && result <= 2, "Quadratic equation has only 0, 1 or 2 real roots.");
+    }
+    
+}
+
+void TestOneRoot() {
+    default_random_engine generator;
+    uniform_real_distribution<double> distribution(-10, 10);
+
+    double x, p, q;
+    int result;
+
+    for (size_t i = 0; i < 100; i++) {
+        x = distribution(generator);
+        p = -(x + x);
+        q = x * x;
+
+        result = GetDistinctRealRootCount(1, p, q);
+
+        stringstream description;
+        description << showpos << "x^2" << p << "x" << q << "= 0 has 1 real root.";
+
+        AssertEqual(result, 1, description.str());
+    }
+    
+}
+
+void TestNoRoots() {
+    AssertEqual(GetDistinctRealRootCount(1, 0, 1), 0,
+        "x^2+1 = 0 has 0 real roots.");
+    AssertEqual(GetDistinctRealRootCount(1, 3, 4), 0,
+        "x^2+3x+4 = 0 has 0 real roots.");
+    AssertEqual(GetDistinctRealRootCount(1, -2, 10), 0,
+        "x^2-2x+10 = 0 has 0 real roots.");
+}
+
+void TestLinearEquation() {
+    AssertEqual(GetDistinctRealRootCount(0, 2, 1), 1,
+        "2x+1 = 0 has 1 real root.");
+    AssertEqual(GetDistinctRealRootCount(0, -1, 0), 1,
+        "-x = 0 has 1 real root.");
+    AssertEqual(GetDistinctRealRootCount(0, 120, -10), 1,
+        "120x - 10 = 0 has 1 real root.");
+}
+
+void TestConstant() {
+    AssertEqual(GetDistinctRealRootCount(0, 0, 1), 0,
+        "c = 0, where c = 1 has 0 real roots.");
+    AssertEqual(GetDistinctRealRootCount(0, 0, -10), 0,
+        "c = 0, where c = -10 has 0 real roots.");
+    AssertEqual(GetDistinctRealRootCount(0, 0, 189238910), 0,
+        "c = 0, where c = 189238910 has 0 real roots.");
+}
 
 
 
@@ -98,4 +187,45 @@ string Join(const Collection& c, char d) {
                 ss << i;
         }
         return ss.str();
+}
+
+
+
+
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const string& hint) {
+    if (t != u) {
+        ostringstream os;
+        os << "Assertion failed: " << t << " != " << u;
+        if (!hint.empty()) {
+             os << " --- hint: " << hint;
+        }
+        throw runtime_error(os.str());
+    }
+}
+
+void Assert(bool b, const string& hint) {
+    AssertEqual(b, true, hint);
+}
+
+template <class TestFunc>
+void TestRunner::RunTest(TestFunc func, const string& test_name) {
+    try {
+        func();
+        cerr << test_name << " OK" << endl;
+    } catch (exception& e) {
+        ++fail_count;
+        cerr << test_name << " fail: " << e.what() << endl;
+    } catch (...) {
+        ++fail_count;
+        cerr << "Unknown exception caught" << endl;
+    }
+}
+
+TestRunner::~TestRunner() {
+    if (fail_count > 0) {
+        cerr << fail_count << " unit tests failed" << endl;
+        exit(1);
+    }
 }
--- names.cpp
+++ names.cpp
@@ -1,62 +1,204 @@
 #include <iostream>
-#include <map>
-#include <set>
 #include <sstream>
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <map>
+#include <set>
+#include <random>
 
 using namespace std;
 
-template <class T>
-ostream& operator << (ostream& os, const vector<T>& s) {
-    os << "{";
-    bool first = true;
-    for (const auto& x : s) {
-        if (!first) {
-            os << ", ";
-        }
-        first = false;
-        os << x;
-    }
-    return os << "}";
-}
+template <typename First, typename Second>
+ostream& operator << (ostream& out, const pair<First, Second>& p);
 
-template <class T>
-ostream& operator << (ostream& os, const set<T>& s) {
-    os << "{";
-    bool first = true;
-    for (const auto& x : s) {
-        if (!first) {
-            os << ", ";
-        }
-        first = false;
-        os << x;
-    }
-    return os << "}";
-}
+template <typename T>
+ostream& operator << (ostream& out, const vector<T>& v);
 
-template <class K, class V>
-ostream& operator << (ostream& os, const map<K, V>& m) {
-    os << "{";
-    bool first = true;
-    for (const auto& kv : m) {
-        if (!first) {
-            os << ", ";
-        }
-        first = false;
-        os << kv.first << ": " << kv.second;
-    }
-    return os << "}";
-}
+template <typename Key, typename Value>
+ostream& operator << (ostream& out, const map<Key, Value>& m);
+
+template <typename Collection>
+string Join(const Collection& c, char d);
+
+
+
+
 
 template<class T, class U>
-void AssertEqual(const T& t, const U& u, const string& hint = {}) {
+void AssertEqual(const T& t, const U& u, const string& hint);
+
+void Assert(bool b, const string& hint);
+
+class TestRunner {
+public:
+    template <class TestFunc>
+    void RunTest(TestFunc func, const string& test_name);
+
+    ~TestRunner();
+
+private:
+    int fail_count = 0;
+};
+
+
+
+
+
+pair<string, string> GetNameSurname(const map<int, pair<string, string>> &data, const int &year) {
+    auto it = --data.end();
+    auto stop = data.begin();
+    pair<string, string> result;
+    while ((it->first > year || it->second.first.empty()) && it != stop)
+        it--;
+    if ((it != stop || !it->second.first.empty()) && it->first <= year)
+        result.first = it->second.first;
+    it = --data.end();
+    while ((it->first > year || it->second.second.empty()) && it != stop)
+        it--;
+    if ((it != stop || !it->second.second.empty()) && it->first <= year)
+        result.second = it->second.second;
+    return result;
+}
+
+class Person {
+public:
+    void ChangeFirstName(int year, const string &first_name) {
+        data[year].first = first_name;
+    }
+
+    void ChangeLastName(int year, const string &last_name) {
+        data[year].second = last_name;
+    }
+
+    string GetFullName(int year) {
+        if (data.size() == 0)
+            return "ERROR";
+        pair<string, string> result = GetNameSurname(data, year);
+        if (result.first.empty() && result.second.empty())
+            return "Incognito";
+        if (!result.first.empty() && !result.second.empty())
+            return result.first + ' ' + result.second;
+        if (!result.first.empty())
+            return result.first + " with unknown last name";
+        return result.second + " with unknown first name";
+    }
+
+private:
+    map<int, pair<string, string>> data;
+};
+
+
+
+
+
+void TestAll();
+
+void Test1();
+void Test2();
+void Test3();
+
+
+
+
+
+int main() {
+    TestAll();
+
+    return 0;
+}
+
+
+
+
+
+void TestAll() {
+    TestRunner runner;
+    runner.RunTest(Test1, "Test1");
+    runner.RunTest(Test2, "Test2");
+    runner.RunTest(Test3, "Test3");
+}
+
+void Test1() {
+    Person person;
+    person.ChangeLastName(1965, "Sergeeva");
+    person.ChangeFirstName(1967, "Polina");
+
+    AssertEqual(person.GetFullName(1964), "Incognito", "1964");
+    AssertEqual(person.GetFullName(1966), "Sergeeva with unknown first name", "1966");
+    AssertEqual(person.GetFullName(1990), "Polina Sergeeva", "1990");
+}
+
+void Test2() {
+    Person person;
+    person.ChangeFirstName(1965, "Polina");
+    person.ChangeLastName(1967, "Sergeeva");
+
+    AssertEqual(person.GetFullName(1900), "Incognito", "1900");
+    AssertEqual(person.GetFullName(1965), "Polina with unknown last name", "1965");
+    AssertEqual(person.GetFullName(1990), "Polina Sergeeva", "1990");
+}
+
+void Test3() {
+    Person person;
+    person.ChangeLastName(1967, "Sergeeva");
+    person.ChangeFirstName(1965, "Polina");
+
+    person.ChangeFirstName(1970, "Appolinaria");
+
+    AssertEqual(person.GetFullName(1969), "Polina Sergeeva", "1969 - change name");
+    AssertEqual(person.GetFullName(1970), "Appolinaria Sergeeva", "1970 - change name");
+
+    person.ChangeLastName(1968, "Volkova");
+
+    AssertEqual(person.GetFullName(1969), "Polina Volkova", "1969 - change surname");
+    AssertEqual(person.GetFullName(1970), "Appolinaria Volkova", "1970 - change surname");
+}
+
+
+
+
+
+template <typename First, typename Second>
+ostream& operator << (ostream& out, const pair<First, Second>& p) {
+        return out << '(' << p.first << ',' << p.second << ')';
+}
+
+template <typename T>
+ostream& operator << (ostream& out, const vector<T>& v) {
+        return out << '[' << Join(v, ',') << ']';
+}
+
+template <typename Key, typename Value>
+ostream& operator << (ostream& out, const map<Key, Value>& m) {
+        return out << '{' << Join(m, ',') << '}';
+}
+
+template <typename Collection>
+string Join(const Collection& c, char d) {
+        stringstream ss;
+        bool first = true;
+        for (const auto& i : c) {
+                if (!first) {
+                        ss << d << ' ';
+                }
+                first = false;
+                ss << i;
+        }
+        return ss.str();
+}
+
+
+
+
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const string& hint) {
     if (t != u) {
         ostringstream os;
         os << "Assertion failed: " << t << " != " << u;
         if (!hint.empty()) {
-            os << " hint: " << hint;
+             os << " --- hint: " << hint;
         }
         throw runtime_error(os.str());
     }
@@ -66,48 +208,23 @@ void Assert(bool b, const string& hint) {
     AssertEqual(b, true, hint);
 }
 
-class TestRunner {
-public:
-    template <class TestFunc>
-    void RunTest(TestFunc func, const string& test_name) {
-        try {
-            func();
-            cerr << test_name << " OK" << endl;
-        } catch (exception& e) {
-            ++fail_count;
-            cerr << test_name << " fail: " << e.what() << endl;
-        } catch (...) {
-            ++fail_count;
-            cerr << "Unknown exception caught" << endl;
-        }
+template <class TestFunc>
+void TestRunner::RunTest(TestFunc func, const string& test_name) {
+    try {
+        func();
+        cerr << test_name << " OK" << endl;
+    } catch (exception& e) {
+        ++fail_count;
+        cerr << test_name << " fail: " << e.what() << endl;
+    } catch (...) {
+        ++fail_count;
+        cerr << "Unknown exception caught" << endl;
     }
+}
 
-    ~TestRunner() {
-        if (fail_count > 0) {
-            cerr << fail_count << " unit tests failed. Terminate" << endl;
-            exit(1);
-        }
+TestRunner::~TestRunner() {
+    if (fail_count > 0) {
+        cerr << fail_count << " unit tests failed" << endl;
+        exit(1);
     }
-
-private:
-    int fail_count = 0;
-};
-
-class Person {
-public:
-    // Вы можете вставлять сюда различные реализации класса,
-    // чтобы проверить, что ваши тесты пропускают корректный код
-    // и ловят некорректный
-    void ChangeFirstName(int year, const string& first_name) {
-    }
-    void ChangeLastName(int year, const string& last_name) {
-    }
-    string GetFullName(int year) {
-    }
-};
-
-int main() {
-    TestRunner runner;
-    // добавьте сюда свои тесты
-    return 0;
 }
